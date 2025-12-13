1aaf32861a8942b09ed08cf66db943dbce63c203
dandodado <jo4326892@gmail.com>
2025-12-13 20:57:13 +0900

    add water

---
+++ Water.js
@@ -0,0 +1,57 @@
+class Water {
+ constructor(x, y, w) {
+this.pos = createVector( x, y);
+this.vel = createVector(0,0);
+this.acc = createVector(0,0);
+this.w = w;
+
+}
+show () {
+noStroke();
+fill(0, 0, 200);
+ellipse(this.pos.x, this.pos.y, this.w, this.w);
+rectMode (CENTER);
+quad(0 ,height/2-15, width, height/2-15, width, height, 0 , height);
+
+}
+applyForce (force) {
+    this.acc.add(force);
+}
+update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+}
+checkEdges() {
+    if (this.pos.y > height/2 - this.w/2) {
+        this.vel.y *= -0.5;
+        this.pos.y = height/2 - this.w/2;
+    }
+     if (this.pos.x > width - this.w/2) {
+        this.vel.x *= -0.5;
+        this.pos.x = width - this.w/2;
+    }
+    if (this.pos.x < 0 + this.w/2) {
+        this.vel.x *= -0.5;
+        this.pos.x = 0 + this.w/2;
+    }
+    
+}
+repel(other) {
+  let dir = p5.Vector.sub(this.pos, other.pos);
+  let d = dir.mag();
+
+  let minDist = this.w*2;
+
+  if (d > 0 && d < minDist) {
+    dir.normalize();
+
+    let strength = (minDist - d) * 0.001;
+    dir.mult(strength);
+
+    this.applyForce(dir);
+  }
+}
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+let waters = [];
+let w = 20; 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1300, 400);
+  for (let y = height/2; y < height; y += w*1.2) {
+    for (let x = 0; x < width; x += w*1.2) {
+      waters.push(new Water(x, y, w));
+    }
+  }
 }
 
 function draw() {
   background(220);
+  for (let i = 0; i < waters.length; i++) {
+    for (let j = 0; j < waters.length; j++) {
+      if (i !== j) {
+        waters[i].repel(waters[j]);
+      }
+    }
+  }
+  for (let water of waters) {
+    water.show();
+    g = createVector(0, 0.1);
+    water.applyForce(g);
+    water.update();
+    water.checkEdges();
+  }
 }
